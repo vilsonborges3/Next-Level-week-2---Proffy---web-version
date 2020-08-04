75398e85021fed22fa83fafe211354a0fe25c8b5
Vilson Borges <vilsonborges98@gmail.com>
2020-08-04 11:18:51 -0300

    setting routes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -25,15 +26,15 @@ function Landing () {
       </div>
       <div className="buttons-container">
         <div className="links-container">
-          <a href="" className="study">
+          <Link to="/give-classes" className="study">
             <img src={giveClassesIcons} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/study" className="give-classes">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas  <img src={purpleHeartIcon} alt="Coração Rocho"/>
